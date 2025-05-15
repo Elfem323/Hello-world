6db7de7239029e4bf190fbc25ea7a3fe3e68df58
ODUKOYA OLUWAFEMI ADELEKE <elfemdigitalworld@gmail.com>
2025-05-15 23:25:20 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from './components/Button'
+
+
+function App() {
+  
+
+  return (
+    <div>
+     <h1>Hello world!</h1>
+     <p>Welcome to the world of React</p>
+     <Button />
+    </div>
+  )
+}
+
+export default App
